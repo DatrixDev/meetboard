ee09c982450e3f40e91c2228205cb0684c3afa7e
quocdat233 <datvq.24it@vku.udn.vn>
2026-09-12 01:06:15 +0700

    Configure backend and MySQL Docker

---
+++ backend/meetboard-api/src/main/java/io/github/datrixdev/meetboard_api/MeetBoardApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.datrixdev.meetboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetBoardApiApplication.class, args);
+	}
+
+}
